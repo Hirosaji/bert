bac3c30382125dd24bc649b5d70ea4a73c51e5c1
Hiroya Kato <hirosaji.biz@gmail.com>
2020-01-27 15:46:22 +0900

    enable body query & refactor

--- server/application.py
+++ server/application.py
@@ -40,54 +40,73 @@ def calc_simlarity(total, each):
     return sims
 
 
-def kijiID2kijiBody(kiji_ID):
-    # get client
-    client = get_apigw_client(APIGW["CLIENT_ID"], APIGW["CLIENT_SECRET"])
+class convert_to_simlarity:
 
-    # create request object (GET method)
-    r = request_search_article(client, kiji_ID)
+    def __init__(self):
+        self.output = {
+            "body": None,
+            "title": None,
+            "link": None,
+            "sim": None
+        }
 
-    raw_title = r.json()["hits"][0]["title"]
-    URL = "https://www.nikkei.com/article/" + r.json()["hits"][0]["kiji_id_enc"]
+    def from_kijiID(self, kiji_ID):
+        # get client
+        client = get_apigw_client(APIGW["CLIENT_ID"], APIGW["CLIENT_SECRET"])
 
-    raw_body = r.json()["hits"][0]["body"]
-    body_ja = list(filter(is_japanese, raw_body.split()))
+        # create request object (GET method)
+        r = request_search_article(client, kiji_ID)
 
-    # body_ja = [
-    #     "宅配便最大手のヤマトホールディングス（HD）の業績回復が遅れている。2019年4～12月期の連結営業利益は500億円台半ばと、前年同期比3割弱減ったようだ。ネット通販で大口顧客が離反した状況を解消できず、宅配便の取扱数が減少した。主戦場のネット通販で後手に回り、17年10月からの値上げを利益につなげられない構図が固定化しつつある。",
-    #     "10～12月期の宅配便の取扱数は3%弱少ない約5億1千万個、4～12月期は0.7%減の約13億9千万個だった。ヤマトは人手不足で配送能力が限界に達したと判断し、17年10月から値上げに合わせて、1年間顧客からの宅配便受注を抑制していた。ただその間に大口顧客がよりコストの安いライバル社への発注を増やし、ヤマトの存在感が以前と比べて低下している。",
-    #     "10～12月期は主力の宅配便事業が減収減益となった可能性がある。値上げ効果を宅配便取扱数の減少が相殺し、コスト削減も追いつかなかった。ヤマトは昨年10月末に今期の予想営業利益を前期比6%増の620億円に下方修正した。この業績予想は宅配便の取扱数を2%強増やす前提となっており、閑散期にあたる1～3月での挽回は容易ではないとの見方が市場では多い。",
-    #     "同社は長距離を運ぶ幹線トラックの積載率を向上させるなど、コスト削減に向けた管理強化を進めている。新設した夕方以降の配送組織「アンカーキャスト」の配送効率が上昇するなど明るい兆しもある。ただ宅配便の安定増はまだ見込めておらず、苦しい事業環境がまだ続きそうだ。"
-    # ]
+        raw_title = r.json()["hits"][0]["title"]
+        self.output["title"] = list(filter(is_japanese, raw_title.split()))
+        self.output["link"] = "https://www.nikkei.com/article/" + r.json()["hits"][0]["kiji_id_enc"]
 
-    # run BERT
-    simlarities = kijiBody2similarity(body_ja)
+        raw_body = r.json()["hits"][0]["body"]
+        self.output["body"] = list(filter(is_japanese, raw_body.split()))
 
-    return {
-        "body": body_ja,
-        "title": list(filter(is_japanese, raw_title.split())),
-        "link": URL,
-        "sim": simlarities
-    }
+        # body_ja = [
+        #     "宅配便最大手のヤマトホールディングス（HD）の業績回復が遅れている。2019年4～12月期の連結営業利益は500億円台半ばと、前年同期比3割弱減ったようだ。ネット通販で大口顧客が離反した状況を解消できず、宅配便の取扱数が減少した。主戦場のネット通販で後手に回り、17年10月からの値上げを利益につなげられない構図が固定化しつつある。",
+        #     "10～12月期の宅配便の取扱数は3%弱少ない約5億1千万個、4～12月期は0.7%減の約13億9千万個だった。ヤマトは人手不足で配送能力が限界に達したと判断し、17年10月から値上げに合わせて、1年間顧客からの宅配便受注を抑制していた。ただその間に大口顧客がよりコストの安いライバル社への発注を増やし、ヤマトの存在感が以前と比べて低下している。",
+        #     "10～12月期は主力の宅配便事業が減収減益となった可能性がある。値上げ効果を宅配便取扱数の減少が相殺し、コスト削減も追いつかなかった。ヤマトは昨年10月末に今期の予想営業利益を前期比6%増の620億円に下方修正した。この業績予想は宅配便の取扱数を2%強増やす前提となっており、閑散期にあたる1～3月での挽回は容易ではないとの見方が市場では多い。",
+        #     "同社は長距離を運ぶ幹線トラックの積載率を向上させるなど、コスト削減に向けた管理強化を進めている。新設した夕方以降の配送組織「アンカーキャスト」の配送効率が上昇するなど明るい兆しもある。ただ宅配便の安定増はまだ見込めておらず、苦しい事業環境がまだ続きそうだ。"
+        # ]
+
+        # run BERT
+        self.output["sims"] = self.kijiBody2similarity()
+
+        return self.output
 
 
-def kijiBody2similarity(kiji_body):
-    input_data = ["".join(kiji_body)] + kiji_body
-    raw_features = get_futures(BERT_PRAMS, input_data)
+    def from_kijiBody(self, kiji_body):
+        self.output["body"] = kiji_body.splitlines()
 
-    cls_features = []
-    for raw_feature in raw_features:
-        cls_feature = list(filter(lambda layer: layer["token"] == "[CLS]", raw_feature["features"]))
-        cls_features.append(cls_feature[0])
+        # run BERT
+        self.output["sims"] = self.kijiBody2similarity()
 
-    simlarities = calc_simlarity(cls_features[1], cls_features[:0] + cls_features[2:])
+        return self.output
 
-    return simlarities
+
+    def kijiBody2similarity(self):
+        kiji_body = self.output["body"]
+        input_data = ["".join(kiji_body)] + kiji_body
+        raw_features = get_futures(BERT_PRAMS, input_data)
+
+        cls_features = []
+        for raw_feature in raw_features:
+            cls_feature = list(filter(lambda layer: layer["token"] == "[CLS]", raw_feature["features"]))
+            cls_features.append(cls_feature[0])
+
+        simlarities = calc_simlarity(cls_features[1], cls_features[:0] + cls_features[2:])
+
+        return simlarities
 
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 application.config["JSON_AS_ASCII"] = False
+
+# set convert_to_simlarity class
+req = convert_to_simlarity()
 
 # enable CORS
 CORS(application)
@@ -129,15 +148,7 @@ application.add_url_rule(
         lambda: jsonify(
             {
                 "id": request.get_json()["kiji_ID"],
-                "context": kijiID2kijiBody(request.get_json()["kiji_ID"]),
-                "data": [
-                    {"item": "lead", "ratio": 0.75},
-                    {"item": "2st", "ratio": 0.64},
-                    {"item": "3nd", "ratio": 0.23},
-                    {"item": "4rd", "ratio": 0.56},
-                    {"item": "5th", "ratio": 0.61},
-                    {"item": "6th", "ratio": 0.31},
-                ],
+                "context": req.from_kijiID(request.get_json()["kiji_ID"]),
             }
         )
     ),
@@ -152,16 +163,7 @@ application.add_url_rule(
         lambda: jsonify(
             {
                 "id": "",
-                "context": "",
-                "data": [
-                    {"item": "total", "ratio": 0.75},
-                    {"item": "1st", "ratio": 0.57},
-                    {"item": "2nd", "ratio": 0.23},
-                    {"item": "3rd", "ratio": 0.56},
-                    {"item": "4th", "ratio": 0.95},
-                    {"item": "5th", "ratio": 0.81},
-                ],
-                "test": kijiBody2similarity(request.get_json()["kiji_body"]),
+                "context": req.from_kijiBody(request.get_json()["kiji_body"]),
             }
         )
     ),
